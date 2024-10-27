068bd226e103d1661ff46ff31ffa7e59bf76e842
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-10-27 01:37:21 -0700

    Added project setup script

---
+++ src/project-setup.py
@@ -0,0 +1,115 @@
+import logging
+import argparse
+import shutil
+import pathlib
+import json
+import sys
+
+import utils
+
+logging.basicConfig(level=logging.DEBUG)
+
+def main():
+    # ---------------------- Argument Setup ---------------------- #
+    parser = argparse.ArgumentParser(
+        prog="Neural Orientation Field Project Setup Helper",
+        description="""
+        Automatically config the data and cache directory for COLMAP 
+        preprocessor and Neural Orientation Field trainer.
+        """
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        default=pathlib.Path("./data/images/"),
+        help="""
+        The input path for the entire pipeline. Should be a directory
+        containing all the images to be processed. The default path is
+        ./data/images/
+        """,
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default=pathlib.Path("./data/output/"),
+        help="""
+        The output path for the training. Should be a directory to store the
+        trained model. The default path is ./data/output/
+        """,
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "-c",
+        "--cache",
+        default=pathlib.Path("./data/cache/"),
+        help="""
+        The cache path for the entire pipeline. Should be a directory storing
+        all the intermediate cache including COLMAP database, etc. The default
+        path is ./data/cache/
+        """,
+        type=pathlib.Path
+    )
+    parser.add_argument(
+        "--clear",
+        action="store_true",
+        help="""
+        If clearing all the existing cache and output directory.
+        """
+    )
+    # --------------------- Argument Parsing --------------------- #
+    args = parser.parse_args()
+    project_config = utils.ProjectConfig(
+        input_path=args.input,
+        output_path=args.output,
+        cache_path=args.cache
+    )
+    clear: bool = args.clear
+    # ---------------------- Project Setup  ---------------------- #
+    # Check input directory.
+    if not project_config.input_path.exists():
+        logging.error("The input directory doesn't exist.")
+        sys.exit(1)
+    if not project_config.input_path.is_dir():
+        logging.error("The input path is not a directory. See --help for " +
+                      "more details")
+        sys.exit(1)
+    # Setup output directory.
+    if project_config.output_path.exists():
+        if not project_config.output_path.is_dir():
+            logging.error("The output path is not a directory. See --help " +
+                          "for more details.")
+            sys.exit(1)
+        if clear:
+            print("The project setup script will remove " + 
+                  f"{str(project_config.output_path.resolve())}, " +
+                  "are you sure to continue?")
+            if not utils.confirm():
+                sys.exit(0)
+            logging.debug("Removing " +
+                         f"{str(project_config.output_path.resolve())}")
+            shutil.rmtree(project_config.output_path)
+            project_config.output_path.mkdir(parents=True)
+    else:
+        project_config.output_path.mkdir(parents=True)
+    # Setup cache directory.
+    if project_config.cache_path.exists():
+        if not project_config.cache_path.is_dir():
+            logging.error("The cache path is not a directory. See --help " +
+                          "for more details.")
+            sys.exit(1)
+        if clear:
+            print("The project setup script will remove " + 
+                  f"{str(project_config.cache_path.resolve())}, " +
+                  "are you sure to continue?")
+            if not utils.confirm():
+                sys.exit(0)
+            logging.debug("Removing " +
+                         f"{str(project_config.cache_path.resolve())}")
+            shutil.rmtree(project_config.cache_path)
+            project_config.cache_path.mkdir(parents=True)
+    else:
+        project_config.cache_path.mkdir(parents=True)
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils.py
@@ -0,0 +1,41 @@
+import pathlib
+from dataclasses import dataclass
+
+@dataclass
+class ProjectConfig:
+    """Neural Orientation Field project config dataclass.
+
+    Attributes: 
+        input_path: the path containing all the input images.
+        output_path: the path to store the trained model.
+        cache_path: the path for cache including COLMAP database, etc.
+    """
+    input_path: pathlib.Path
+    output_path: pathlib.Path
+    cache_path: pathlib.Path
+
+    def to_dict(self):
+        return {
+            "input_path": str(self.input_path.resolve()),
+            "output_path": str(self.output_path.resolve()),
+            "cache_path": str(self.cache_path.resolve()),
+        }
+
+    @staticmethod
+    def from_dict(config_dict: dict):
+        return ProjectConfig(
+            input_path=pathlib.Path(config_dict["input_path"]),
+            output_path=pathlib.Path(config_dict["output_path"]),
+            cache_path=pathlib.Path(config_dict["cache_path"])
+        )
+
+def confirm():
+    """Ask for confirm.
+
+    Returns:
+        True if confirmed.
+    """
+    answer = ""
+    while answer not in ["y", "n"]:
+        answer = input("OK to push to continue [Y/N]? ").lower()
+    return answer == "y"
