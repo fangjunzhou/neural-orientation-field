a2a345c24e8843f8309abff753db92dd492a6031
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-12-02 17:01:53 -0800

    Added hierarchical volume sampling

--- src/neural_orientation_field/nerf/model.py
+++ src/neural_orientation_field/nerf/model.py
@@ -2,9 +2,14 @@ import torch
 import torch.nn as nn
 
 
-class NerfModel(nn.Module):
-    def __init__(self, filter_size=128, num_encoding_functions=6):
-        super(NerfModel, self).__init__()
+class NeRFModel(nn.Module):
+    def __init__(self):
+        super(NeRFModel, self).__init__()
+
+
+class NeRfCoarseModel(NeRFModel):
+    def __init__(self, filter_size=128, num_encoding_functions=4):
+        super(NeRfCoarseModel, self).__init__()
 
         def init_weights(m):
             if isinstance(m, nn.Linear):
@@ -40,4 +45,58 @@ class NerfModel(nn.Module):
         x = self.relu(self.layer1(input))
         x = self.layer2(x)
         x = self.layer3(x)
+        return x
+
+
+class NeRfFineModel(NeRFModel):
+    def __init__(self, filter_size=128, num_encoding_functions=8):
+        super(NeRfFineModel, self).__init__()
+
+        def init_weights(m):
+            if isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+        # Input layer (default: 42 -> 128)
+        # 3 (position) + 3 (direction) + 3 * 2 * 6 (6 sin and cos encoding for position only)
+        self.layer1 = nn.Linear(
+            3 * 2 + 3 * 2 * num_encoding_functions,
+            filter_size
+        )
+        self.layer1.apply(init_weights)
+        # Layer 2 (default: 128 -> 128)
+        self.layer2 = nn.Sequential(
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+        )
+        # Layer 3 (default: 128 + 42 -> 128)
+        self.layer3 = nn.Sequential(
+            nn.Linear(filter_size + 3 * 2 + 3 * 2 *
+                      num_encoding_functions, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+            nn.Linear(filter_size, filter_size),
+            nn.ReLU(),
+        )
+        self.layer3.apply(init_weights)
+        # Layer 4 (default: 128 -> 4)
+        self.layer4 = nn.Linear(filter_size, 4)
+        self.layer4.apply(init_weights)
+        # Short hand for nn.functional.relu
+        self.relu = nn.functional.relu
+
+    def forward(self, input: torch.Tensor):
+        x = self.relu(self.layer1(input))
+        x = self.layer2(x)
+        x = self.layer3(torch.concat((x, input), dim=-1))
+        x = self.layer4(x)
         return x
--- src/neural_orientation_field/nerf/utils.py
+++ src/neural_orientation_field/nerf/utils.py
@@ -1,6 +1,6 @@
 import torch
 
-from neural_orientation_field.nerf.model import NerfModel
+from neural_orientation_field.nerf.model import NeRFModel
 
 
 def pos_encode(x: torch.Tensor, num_encoding_functions: int = 6, device: torch.device = torch.device("cpu")):
@@ -21,8 +21,8 @@ def pos_encode(x: torch.Tensor, num_encoding_functions: int = 6, device: torch.d
     return x
 
 
-def volumetric_renderer(
-    model: NerfModel,
+def static_volumetric_renderer(
+    model: NeRFModel,
     camera_origs: torch.Tensor,
     camera_rays: torch.Tensor,
     nc: float,
@@ -57,27 +57,119 @@ def volumetric_renderer(
     sample_depths = sample_depths + \
         (torch.rand(sample_depths.shape, device=device) - 0.5) * \
         interval * sample_jitter
+
+    # Sample from NeRF.
+    radiance = sample_radiance(
+        model, camera_origs, camera_rays, sample_depths[:, :-1], num_pos_encode, device)
+
+    # Integrate color.
+    sample_depths_diff = sample_depths[:, 1:] - sample_depths[:, :-1]
+    occupancy = radiance[:, :, 3]
+    color = radiance[:, :, 0:3] * \
+        occupancy.unsqueeze(-1) * sample_depths_diff[:, :, torch.newaxis]
+    color = torch.sum(color, 1)
+    color = torch.sigmoid(color)
+    return color, occupancy, sample_depths
+
+
+def adaptive_volumetric_renderer(
+    model: NeRFModel,
+    camera_origs: torch.Tensor,
+    camera_rays: torch.Tensor,
+    occupancy: torch.Tensor,
+    sample_depths: torch.Tensor,
+    max_subd_sample: int = 2,
+    num_pos_encode: int = 6,
+    device: torch.device = torch.device("cpu")
+):
+    sample_depths = adaptive_sample_depth(
+        occupancy,
+        sample_depths,
+        max_subd_sample,
+        device
+    )
+    # Sample from NeRF.
+    radiance = sample_radiance(
+        model, camera_origs, camera_rays, sample_depths[:, :-1], num_pos_encode, device)
+
+    # Integrate color.
+    sample_depths_diff = sample_depths[:, 1:] - sample_depths[:, :-1]
+    occupancy = torch.sigmoid(radiance[:, :, 3])
+    color = radiance[:, :, 0:3] * \
+        occupancy.unsqueeze(-1) * sample_depths_diff[:, :, torch.newaxis]
+    color = torch.sum(color, 1)
+    color = torch.sigmoid(color)
+    return color, occupancy, sample_depths
+
+
+def adaptive_sample_depth(
+    occupancy: torch.Tensor,
+    sample_depths: torch.Tensor,
+    max_subd_sample: int = 2,
+    device: torch.device = torch.device("cpu")
+):
+    """Generate adaptive sample depth from depth and occupancy.
+
+    Args:
+        occupancy: occupancy to the corresponding depth.
+        sample_depths: sample depths.
+        num_subdiv_sample: maximum number of subdivision to use.
+
+    Returns:
+        subdivided sample depth.
+    """
+    batch_size, _ = sample_depths.shape
+    # Get the occupancy rank.
+    _, occupancy_rank = torch.sort(occupancy[:, :-1], dim=-1, descending=True)
+    for i in range(max_subd_sample):
+        depth_idx = occupancy_rank[:, i]
+        subd = max_subd_sample - i
+        depth_start = sample_depths[torch.arange(
+            0, batch_size), depth_idx].unsqueeze(1)
+        depth_end = sample_depths[torch.arange(
+            0, batch_size), depth_idx + 1].unsqueeze(1)
+        steps = torch.linspace(0, 1, subd + 2).unsqueeze(0).to(device)
+        subd_depth = depth_start + (depth_end - depth_start) * steps
+        sample_depths = torch.concat(
+            [sample_depths, subd_depth[:, 1:-1]], dim=-1)
+    sample_depths, _ = torch.sort(sample_depths, dim=-1)
+    return sample_depths
+
+
+def sample_radiance(
+    model: NeRFModel,
+    camera_origs: torch.Tensor,
+    camera_rays: torch.Tensor,
+    sample_depths: torch.Tensor,
+    num_pos_encode: int = 6,
+    device: torch.device = torch.device("cpu")
+):
+    """Sample radiance from NeRF model.
+
+    Args:
+        model: NeRF model.
+        camera_origs: camera origins.
+        camera_rays: camera ray directions.
+        num_sample: sample num for each camera ray.
+        num_pos_encode: number of position encoding to use.
+        device: device to use.
+
+    Returns:
+        radiance tensor.
+    """
+    batch_size, _ = camera_origs.shape
+    num_sample = sample_depths.shape[1]
+    # Get sample positions and directions.
     sample_pos = camera_origs[:, torch.newaxis, :] + camera_rays[:,
                                                                  torch.newaxis, :] * sample_depths[:, :, torch.newaxis]
     sample_direct = camera_rays.unsqueeze(1).expand((-1, num_sample, -1))
-
     # Send samples to NeRF.
     sample_pos = sample_pos.reshape(-1, 3)
     sample_direct = sample_direct.reshape(-1, 3)
     sample_pos_encode = pos_encode(sample_pos, num_pos_encode, device=device)
-
     # Eval radiance from NeRF
     nerf_input = torch.concat((sample_pos.unsqueeze(1), sample_direct.unsqueeze(
-        1), sample_pos_encode), 1).reshape(n * num_sample, -1)
+        1), sample_pos_encode), 1).reshape(batch_size * num_sample, -1)
     radiance = model(nerf_input)
-    radiance = radiance.reshape(n, num_sample, -1)
-
-    # Integrate color.
-    sample_depths_diff = sample_depths[:, 1:] - sample_depths[:, :-1]
-    sample_depths_diff = torch.concat(
-        (sample_depths_diff, torch.ones((n, 1), device=device) * interval), dim=1)
-    color = radiance[:, :, 0:3] * radiance[:, :,
-                                           3].unsqueeze(-1) * sample_depths_diff[:, :, torch.newaxis]
-    color = torch.sum(color, 1)
-    color = torch.sigmoid(color)
-    return color
+    radiance = radiance.reshape(batch_size, num_sample, -1)
+    return radiance
