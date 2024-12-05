1ce5ada076b44af1a0660aa8012a5602ae12d708
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-12-05 21:51:42 +0000

    Update NeOF training config

--- src/neural_orientation_field/neof/training_config.py
+++ src/neural_orientation_field/neof/training_config.py
@@ -5,17 +5,17 @@ from dataclasses import dataclass
 class NeOFTrainingConfig:
     # Near/far clipping distance.
     nc: int = 1
-    fc: int = 8
+    fc: int = 10
     # Positional encoding size.
     coarse_pos_encode: int = 6
     fine_pos_encode: int = 8
     # Camera ray sample rate.
-    samples_per_ray: int = 16
+    samples_per_ray: int = 24
     max_subd_samples: int = 8
     # Hyper parameters.
     lr: float = 2e-4
-    num_epoch: int = 2
-    ray_batch_size: int = 4096
+    num_epoch: int = 8
+    ray_batch_size: int = 8192
     # Misc.
     size_train_ray = 1
     num_valid_image = 2
